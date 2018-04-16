1de78bb0aa2cb0538d02e1e00dfcff6c79b08d29
Joseph <Jose.perez@peluka.info>
2018-04-16 19:01:03 -0300

    actualizacion clases

---
+++ Aplicacion/Estado.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicacion
+{
+    class Estado
+    {
+        private int codReso { get; set; }
+        private string estadoResolucion { get; set; }
+        private string motivoResolucion { get; set; }
+
+
+        public Estado()
+        {
+            codReso = 0;
+            estadoResolucion = string.Empty;
+            motivoResolucion = string.Empty;
+        }
+    }
+}
---
+++ Aplicacion/SolicitudPermiso.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicacion
+{
+    class SolicitudPermiso
+    {
+        private int codigoPermiso { get; set; }
+        private int diasSolicitados { get; set; }
+        private DateTime fechaInicio { get; set; }
+        private DateTime fechaTermino { get; set; }
+        private string motivoSolicitud { get; set; }
+
+        public SolicitudPermiso()
+        {
+            codigoPermiso = 0;
+            diasSolicitados = 0;
+            fechaInicio = DateTime.Today;
+            fechaTermino = DateTime.Today;
+            motivoSolicitud = string.Empty;
+        }
+
+        public SolicitudPermiso()
+        {
+
+        }
+    }
+}
